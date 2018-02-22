9f79963adc5de1bd923b30cb73d356704d126501
BaconBett <micheltantsch@gmail.com>
2018-02-23 00:01:29 +0100

    bot token is now a start argument
     token now has to be given as an argument when running the .jar

--- The Jeffer/src/discord/main/botMain.java
+++ The Jeffer/src/discord/main/botMain.java
@@ -7,18 +7,25 @@ import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.util.DiscordException;
 
 public class botMain {
-
-	 public static final IDiscordClient bot = createClient("NDEyMDIwOTAzNjA2NzQ3MTQ3.DWEL8w.IXPFabAVbA4NDyEajUiXHJIrZpc", true);
+	
+	
+	 //public static final IDiscordClient bot = createClient("NDEyMDIwOTAzNjA2NzQ3MTQ3.DWEL8w.IXPFabAVbA4NDyEajUiXHJIrZpc", true);
 	
 	 public static void main(String args[]) {
 		 
+		 if (args.length<1) {
+			 System.out.println("you need to provide your bot token ! ");
+			 System.out.println("java -jar theJeffer [Your Token here]");
+			 return;
+		 }
+		 IDiscordClient bot = createClient(args[0], true);
 		 EventDispatcher dis = bot.getDispatcher();
 		 dis.registerListener(new botListener());
 		 
 	 }
 	 
-	 public static IDiscordClient createClient(String token, boolean login)
-	    {
+	 public static IDiscordClient createClient(String token, boolean login){
+		 
 	        ClientBuilder clientBuilder = new ClientBuilder();
 	        clientBuilder.withToken(token);
 	        try
